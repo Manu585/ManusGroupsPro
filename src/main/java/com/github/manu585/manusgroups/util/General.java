fb001d44e927c7c0f82e0fac325440bad0319788
Manu585 <manuelgogg2@gmail.com>
2025-10-06 11:02:08 +0200

    Fixed insufferable #isSign(Block block) method to use getState() and Tag checks

--- src/main/java/com/github/manu585/manusgroups/util/General.java
+++ src/main/java/com/github/manu585/manusgroups/util/General.java
@@ -1,6 +1,7 @@
 package com.github.manu585.manusgroups.util;
 
 import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -54,18 +55,9 @@ public final class General {
 
     public static boolean isSign(Block block) {
         if (block == null) return false;
+        if (block.getState() instanceof Sign) return true;
+
         final Material type = block.getType();
-        return block.getState() instanceof Sign ||
-                type == Material.OAK_SIGN || type == Material.OAK_WALL_SIGN ||
-                type == Material.SPRUCE_SIGN || type == Material.SPRUCE_WALL_SIGN ||
-                type == Material.BIRCH_SIGN || type == Material.BIRCH_WALL_SIGN ||
-                type == Material.JUNGLE_SIGN || type == Material.JUNGLE_WALL_SIGN ||
-                type == Material.ACACIA_SIGN || type == Material.ACACIA_WALL_SIGN ||
-                type == Material.DARK_OAK_SIGN || type == Material.DARK_OAK_WALL_SIGN ||
-                type == Material.CRIMSON_SIGN || type == Material.CRIMSON_WALL_SIGN ||
-                type == Material.WARPED_SIGN || type == Material.WARPED_WALL_SIGN ||
-                type == Material.MANGROVE_SIGN || type == Material.MANGROVE_WALL_SIGN ||
-                type == Material.BAMBOO_SIGN || type == Material.BAMBOO_WALL_SIGN ||
-                type == Material.CHERRY_SIGN || type == Material.CHERRY_WALL_SIGN;
+        return Tag.SIGNS.isTagged(type) || Tag.WALL_SIGNS.isTagged(type) || Tag.WALL_HANGING_SIGNS.isTagged(type);
     }
 }
